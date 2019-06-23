51ea8e04fc27b778bf634164e3960066827a907d
Brett McDonald <bldmcdonald@gmail.com>
2019-06-23 17:54:15 +1200

    Section 7 complete

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -8,38 +8,42 @@ A view showing the details for a landmark.
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
 
-            CircleImage()
-                .offset(x: 0, y: -130)
+            CircleImage(image: landmark.image(forSize: 250))
+                .offset(y: -130)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
-                HStack(alignment: .top) {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
+                
+            HStack(alignment: .top) {
+                Text(landmark.park)
+                    .font(.subheadline)
+                Spacer()
+                Text(landmark.state)
+                    .font(.subheadline)
                 }
             }
             .padding()
 
             Spacer()
         }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 #if DEBUG
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
 #endif
--- Landmarks/Supporting Views/LandmarkList.swift
+++ Landmarks/Supporting Views/LandmarkList.swift
@@ -10,9 +10,13 @@ import SwiftUI
 
 struct LandmarkList : View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarkData[0])
-            LandmarkRow(landmark: landmarkData[1])
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
